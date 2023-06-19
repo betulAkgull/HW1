e947fe41e7b2842e981fbce3b09b30e2b4ac6963
Betül Akgül <76072632+betulAkgull@users.noreply.github.com>
2023-06-19 20:04:25 +0000

    new

--- hw1.kt
+++ hw1.kt
@@ -1,3 +1,23 @@
-fun main () {
-    
+fun main() {
+     guessGame()
+}
+
+fun guessGame(){
+    println("Find the random number by guessing, there are 3 chances.")
+    val input = Scanner(System.`in`)
+    val rnd = (0..101).random()
+    println("random: $rnd")
+     for(i in 3 downTo 1){
+            println("Make a guess: ")
+            val guess = input.nextInt()
+            if(guess.equals(rnd)){
+                println("You find it, Congrats.")
+                break
+            }else if(guess > rnd){
+                println("Make it smaller, there are ${i-1} guesses")
+            }else{
+                println("Make it bigger, there are ${i-1} guesses")
+         }
+     }
+     
 }
