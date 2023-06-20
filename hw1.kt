b4f741629be23b82f4fbe6db9f8874bb42b36106
Betül Akgül <76072632+betulAkgull@users.noreply.github.com>
2023-06-20 22:16:27 +0000

    final

--- hw1.kt
+++ hw1.kt
@@ -1,23 +1,29 @@
 fun main() {
-     guessGame()
+    guessGame()
 }
 
-fun guessGame(){
-    println("Find the random number by guessing, there are 3 chances.")
-    val input = Scanner(System.`in`)
+fun guessGame() {
     val rnd = (0..101).random()
-    println("random: $rnd")
-     for(i in 3 downTo 1){
-            println("Make a guess: ")
-            val guess = input.nextInt()
-            if(guess.equals(rnd)){
-                println("You find it, Congrats.")
-                break
-            }else if(guess > rnd){
-                println("Make it smaller, there are ${i-1} guesses")
-            }else{
-                println("Make it bigger, there are ${i-1} guesses")
-         }
-     }
-     
+    val input = Scanner(System.`in`)
+    println("Welcome to the Guessing Game!")
+    println("I have chosen a number between 1 and 101. Can you guess it?")
+
+    for (attempts in 1..3) {
+        print("Attempt $attempts: Enter your guess: ")
+        val guess = input.nextInt()
+
+        if (guess == rnd) {
+            println("Congratulations! You guessed the number in $attempts attempts.")
+            return
+        } else if (guess != null) {
+            if (guess < rnd) {
+                println("Too low!")
+            } else {
+                println("Too high!")
+            }
+        }
+    }
+
+    println("Sorry, you did not guess the number within 3 attempts. The number was $rnd.")
+    println("Thank you for playing!")
 }
